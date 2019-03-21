9423d460a63f5ed25e63a90770ddf444ed613b43
Stefan Verweij <stefan@evently.nl>
2019-03-21 13:03:51 +0100

    add helper to create list of internal question codes

--- src/Traits/LimesurveyRemoteHelperTrait.php
+++ src/Traits/LimesurveyRemoteHelperTrait.php
@@ -18,4 +18,26 @@ trait LimesurveyRemoteHelperTrait
         return $this->exportTimeline('day', $start, $end);
     }
 
+
+    public function getInternalQuestionCode($questionCodeStringOrArray)
+    {
+        //if string create array, else just set array
+        $returnArray = [];
+        $questions = (is_string($questionCodeStringOrArray)) ? array($questionCodeStringOrArray) : $questionCodeStringOrArray;
+        //get questionList from survey
+        $questionList = $this->listQuestions();
+        //go over the questionlist and create the subquestions
+        foreach ($questionList as $question) 
+        {
+            if(in_array($question['title'], $questions))
+            {
+                $returnArray[$question['title']] = "{$question['sid']}X{$question['gid']}X{$question['qid']}";
+            }
+        }
+        asort($returnArray);
+        return ($returnArray);
+
+
+    }
+
 }
